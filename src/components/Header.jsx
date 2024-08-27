fe48d13ba66392cf3b980f09060234422fefad06
VanceGoblin <zvkubajak@gmail.com>
2024-08-27 13:45:41 -0500

    Refined Transition Effects and Customized Name Text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ import Nav from './Navigation.jsx';
 export default function App() {
   return (
     <header>
-      <h1>
-        ZVKubajak
+      <h1 href="google.com">
+        <a href="#"><span>ZVK</span>ubajak</a>
       </h1>
       <Nav />
     </header>
