04d821cc357cf474388a39be84c72358fa4c0ad9
VanceGoblin <zvkubajak@gmail.com>
2024-08-28 13:09:24 -0500

    Added More to About Me Page

--- src/react/pages/AboutMe.jsx
+++ src/react/pages/AboutMe.jsx
@@ -1,5 +1,4 @@
 import '../../css/css_pages/aboutMe.css';
-// import image from '../../images/IMG_0623.JPG';
 
 export default function AboutMe() {
   return (
@@ -9,6 +8,15 @@ export default function AboutMe() {
         <p>
           Hello, my name is Zander, and I am a full steck web developer. I like to work on cars, experience new things, and program applications for myself to use everyday. I have experience in the following languages:
         </p>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>TypeScript</li>
+          <li>NodeJS</li>
+          <li>React</li>
+          <li>PostgreSQL</li>
+        </ul>
       </article>
       <img src="https://placehold.co/300x300"></img>
     </section>
