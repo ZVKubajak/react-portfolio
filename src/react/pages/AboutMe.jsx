8cd67ec0610ee0ca3a2cf6a5557bee0f1e503f8a
VanceGoblin <zvkubajak@gmail.com>
2024-08-28 07:55:46 -0500

    Added Content and Style to About Me Page

--- src/react/pages/AboutMe.jsx
+++ src/react/pages/AboutMe.jsx
@@ -1,15 +1,16 @@
 import '../../css/css_pages/aboutMe.css';
+// import image from '../../images/IMG_0623.JPG';
 
 export default function AboutMe() {
   return (
     <section>
-      <img src="https://placehold.co/200x200"></img>
       <article>
         <h1>About Me</h1>
         <p>
-          Hello World
+          Hello, my name is Zander, and I am a full steck web developer. I like to work on cars, experience new things, and program applications for myself to use everyday. I have experience in the following languages:
         </p>
       </article>
+      <img src="https://placehold.co/300x300"></img>
     </section>
   );
 }
