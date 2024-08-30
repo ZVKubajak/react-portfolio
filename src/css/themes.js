88d8b6dd833a3f7cbf732bbf6b3ff16915d72673
VanceGoblin <zvkubajak@gmail.com>
2024-08-30 11:36:19 -0500

    Test Commit for Theme Provider

---
+++ src/css/themes.js
@@ -0,0 +1,8 @@
+import { createTheme } from 'react-bootstrap';
+
+const theme = createTheme({
+  primary: 'red',
+  secondary: 'orange'
+});
+
+export default theme;
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,11 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { ThemeProvider } from 'react-bootstrap';
+import theme from './css/themes.js';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>
+);
