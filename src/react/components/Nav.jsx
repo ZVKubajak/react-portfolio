35d16d2b3a236730909461ca018cbdd7da78d275
VanceGoblin <zvkubajak@gmail.com>
2024-09-03 08:39:52 -0500

    Updated Nav.jsx With React Router

--- src/react/components/Nav.jsx
+++ src/react/components/Nav.jsx
@@ -1,13 +1,44 @@
 import '../../css/css_components/nav.css';
+import { Link, useLocation } from 'react-router-dom';
 
 export default function Nav() {
+  const currentPage = useLocation().pathname;
+
   return (
     <nav>
       <ul>
-        <li><a href="#">ABOUT ME</a></li>
-        <li><a href="#">PORTFOLIO</a></li>
-        <li><a href="#">CONTACT</a></li>
-        <li><a href="#">RESUME</a></li>
+        <li>
+          <Link
+            to="/"
+            className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
+          >
+            ABOUT ME
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/Portfolio"
+            className={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}
+          >
+            PORTFOLIO
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/Contact"
+            className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
+          >
+            CONTACT
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/Resume"
+            className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}
+          >
+            RESUME
+          </Link>
+        </li>
       </ul>
     </nav>
   );
