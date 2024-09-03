a6af0caa85444e795ab7b951a393c01fdc49f19b
VanceGoblin <zvkubajak@gmail.com>
2024-09-03 08:25:16 -0500

    Added Router to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,39 @@
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+import App from './App.jsx';
+import AboutMe from './AboutMe.jsx';
+import Contact from './Contact.jsx';
+import Portfolio from './Portfolio.jsx';
+import Resume from './Resume.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <AboutMe />
+      },
+      {
+        path: '/Contact',
+        element: <Contact />
+      },
+      {
+        path: '/Portfolio',
+        element: <Portfolio />
+      },
+      {
+        path: '/Resume',
+        element: <Resume />
+      }
+    ]
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
